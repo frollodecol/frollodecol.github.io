49361a984a6bf6946af62938f52e1810a4f4bdb4
frollodecol <frollodecol@gmail.com>
2016-06-23 22:21:52 +0600

    Add Sedona

---
+++ sedona/gulpfile.js
@@ -0,0 +1,148 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var pug = require('gulp-pug');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence');
+var spritesmith = require('gulp.spritesmith');
+
+// Basic Gulp task syntax
+gulp.task('hello', function() {
+  console.log('Hello Zell!');
+})
+
+// Development Tasks 
+// -----------------
+
+// Start browserSync server
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  })
+})
+
+gulp.task('sass', function() {
+  return gulp.src('app/scss/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+    .pipe(sass()) // Passes it through a gulp-sass
+    .pipe(gulp.dest('app/css')) // Outputs it in the css folder
+    .pipe(browserSync.reload({ // Reloading with Browser Sync
+      stream: true
+    }));
+})
+
+gulp.task('pug', function() {
+  return gulp.src('app/pug/*.pug') // Gets all files ending with .scss in app/scss and children dirs
+    .pipe(pug({
+      pretty: true
+    })) // Passes it through a gulp-sass
+    .pipe(gulp.dest('app/')) // Outputs it in the css folder
+    .pipe(browserSync.reload({ // Reloading with Browser Sync
+      stream: true
+    }));
+})
+
+// Watchers
+gulp.task('watch', function() {
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/pug/*.pug', ['pug']);
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+})
+
+// Optimization Tasks 
+// ------------------
+
+// Optimizing CSS and JavaScript 
+gulp.task('useref', function() {
+
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'));
+});
+
+// Optimizing Images 
+gulp.task('images', function() {
+  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+    // Caching images that ran through imagemin
+    .pipe(cache(imagemin({
+      interlaced: true,
+    })))
+    .pipe(gulp.dest('dist/images'))
+});
+
+// Copying fonts 
+gulp.task('fonts', function() {
+  return gulp.src('app/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'))
+})
+
+// Cleaning 
+gulp.task('clean', function() {
+  return del.sync('dist').then(function(cb) {
+    return cache.clearAll(cb);
+  });
+})
+
+gulp.task('clean:dist', function() {
+  return del.sync(['dist/**/*', '!dist/images', '!dist/images/**/*']);
+});
+
+//Autoprefixer
+
+gulp.task('autoprefixer', function () {
+  return gulp.src('app/scss/*.scss')
+    .pipe(autoprefixer({
+      browsers: ['> 0%'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('app/css'));
+});
+
+//Спрайты 
+
+gulp.task('sprite', function() {
+    var spriteData = 
+        gulp.src('app/images/sprite/*.*') // путь, откуда берем картинки для спрайта
+            .pipe(spritesmith({
+                imgName: 'sprite.png',
+                cssName: 'sprite.scss',
+                imgPath: '../images/sprite.png',
+            }));
+
+    spriteData.img.pipe(gulp.dest('app/images/')); // путь, куда сохраняем картинку
+    spriteData.css.pipe(gulp.dest('app/scss/components')); // путь, куда сохраняем стили
+});
+
+// Build Sequences
+// ---------------
+
+gulp.task('default', function(callback) {
+  runSequence(['sass', 'pug', 'autoprefixer', 'browserSync', 'watch'],
+    callback
+  )
+})
+
+gulp.task('build', function(callback) {
+  runSequence(
+    'clean:dist', 'sprite', 
+    ['sass','pug', 'autoprefixer', 'useref', 'images', 'fonts'],
+    callback
+  )
+})
+
+/* <!--build:js js/main.min.js -->
+<!-- endbuild -->
+*/
+
