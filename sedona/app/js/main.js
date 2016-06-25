7a0b3595cf630bcab618a7142c04fc87f1415faf
frollodecol <frollodecol@gmail.com>
2016-06-25 11:46:10 +0600

    Add footer

--- sedona/app/js/main.js
+++ sedona/app/js/main.js
@@ -3,7 +3,7 @@ function initialize() {
   center:new google.maps.LatLng(34.869540, -111.762154),
   zoom: 16,
   mapTypeId: google.maps.MapTypeId.ROADMAP,
-  disableDefaultUI: true
+  disableDefaultUI: true,
   };
   var map = new google.maps.Map(document.getElementById("map__google-map"), mapProp);
 }
