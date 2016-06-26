eb6637eeda05e79149e86b1fa4a2b10469dee29e
frollodecol <frollodecol@gmail.com>
2016-06-26 14:32:46 +0600

    Add responsive 5

--- sedona/app/js/main.js
+++ sedona/app/js/main.js
@@ -1,3 +1,8 @@
+function centeringAbsolutePositionElement() {
+  setTimeout( function() {
+    hotelForm.style.marginLeft = (container.clientWidth - hotelForm.clientWidth) / 2 + 'px';
+  }, 200);  
+};
 function initialize() {
   var mapProp = {
   center:new google.maps.LatLng(34.869540, -111.762154),
@@ -6,16 +11,19 @@ function initialize() {
   disableDefaultUI: true,
   };
   var map = new google.maps.Map(document.getElementById("map__google-map"), mapProp);
-}
+};
 google.maps.event.addDomListener(window, 'load', initialize);
 
-var minusAdults = document.querySelector('.hotel-form__icon-minus--adults');
+var minusAdults = document.querySelector('.hotel-form__minus-adults');
 var inputAdults = document.querySelector('.hotel-form__input--adults');
-var plusAdults = document.querySelector('.hotel-form__icon-plus--adults');
+var plusAdults = document.querySelector('.hotel-form__plus-adults');
 
-var minusChildren = document.querySelector('.hotel-form__icon-minus--children');
+var minusChildren = document.querySelector('.hotel-form__minus-children');
 var inputChildren = document.querySelector('.hotel-form__input--children');
-var plusChildren = document.querySelector('.hotel-form__icon-plus--children');
+var plusChildren = document.querySelector('.hotel-form__plus-children');
+
+var hotelForm = document.querySelector('.hotel-form');
+var container = document.querySelector('.container');
 
 minusAdults.addEventListener('click', function(event) {
   event.preventDefault();
@@ -40,3 +48,7 @@ plusChildren.addEventListener('click', function(event) {
   event.preventDefault();
     inputChildren.value = +inputChildren.value + 1;
 });
+
+window.onload = centeringAbsolutePositionElement;
+
+window.onresize = centeringAbsolutePositionElement; 
